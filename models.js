72f768c969824c031da54f0a4f5dadaca6b3048b
LarissaMorrell <larissa@larissamorrell.com>
2017-01-30 15:25:18 -0500

    updated after solution

--- models.js
+++ models.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
 const blogPostSchema = mongoose.Schema({
-  author: {
-    firstName: String,
-    lastName: String
-  },
+  author: { firstName: String, lastName: String},
   title: {type: String, required: true},
   content: {type: String},
   created: {type: Date, default: Date.now}
