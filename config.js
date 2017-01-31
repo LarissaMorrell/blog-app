7c34756c56cc97df66c638c2f52dd6f66547525d
LarissaMorrell <larissa@larissamorrell.com>
2017-01-30 20:46:50 -0500

    still need to retrieve data from mLab

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://dbuser:dbpassword@ds137749.mlab.com:37749/blog';
+                       'mongodb://dbuser:dbpassword@ds137749.mlab.com:37749/blog';
 exports.PORT = process.env.PORT || 8080;
