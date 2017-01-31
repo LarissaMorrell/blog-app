92c7da882e8e9f031877d7a73b6df4e0843018fb
LarissaMorrell <larissa@larissamorrell.com>
2017-01-30 19:52:11 -0500

    config update

--- config.js
+++ config.js
@@ -1,4 +1,2 @@
-exports.DATABASE_URL = process.env.DATABASE_URL ||
-   //                    global.DATABASE_URL ||
-                      'mongodb://dbuser:dbpassword@ds137749.mlab.com:37749/blog';
+exports.DATABASE_URL = 'mongodb://dbuser:dbpassword@ds137749.mlab.com:37749/blog';
 exports.PORT = process.env.PORT || 8080;
